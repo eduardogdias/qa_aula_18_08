83372b31e7709eade6b60189724d24ed2a3f0ab4
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-08-25 00:43:02 -0300

    implementação de todas as categorias de Apdex

--- Apdex/src/test/java/example/ApdexTest.java
+++ Apdex/src/test/java/example/ApdexTest.java
@@ -14,18 +14,46 @@ public class ApdexTest {
         //act
         double score = apdex.calculaApdex(1000, 0, 1000);
         //assert
-        Assertions.assertEquals(1, score);
+        Assertions.assertEquals(1, score, 0.01);
     }
 
+    @Test
+    public void validarApdexBom(){
+        //arrange
+        Apdex apdex = new Apdex();
+        //act
+        double score = apdex.calculaApdex(870, 40, 1000);
+        //assert
+        Assertions.assertEquals(0.89, score,0.01);
+    }
 
     @Test
     public void validarApdexRazoavel(){
         //arrange
         Apdex apdex = new Apdex();
         //act
-        double score = apdex.calculaApdex(270, 70, 400);
+        double score = apdex.calculaApdex(770, 60, 1000);
         //assert
-        Assertions.assertEquals(0.76, score,0.01);
+        Assertions.assertEquals(0.80, score,0.01);
     }
 
+    @Test
+    public void validarApdexRuim(){
+        //arrange
+        Apdex apdex = new Apdex();
+        //act
+        double score = apdex.calculaApdex(590, 0, 1000);
+        //assert
+        Assertions.assertEquals(0.59, score,0.01);
+    }
+
+    @Test
+    public void validarApdexInaceitavel(){
+        //arrange
+        Apdex apdex = new Apdex();
+        //act
+        double score = apdex.calculaApdex(230, 460, 1000);
+        //assert
+        Assertions.assertEquals(0.46, score,0.01);
+    }
 }
